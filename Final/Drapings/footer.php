38d9b82454576f51f477f8ce9f1436e0e524e7fa
KevinColbyStrong <Kevin.colby.strong@gmail.com>
2019-03-18 21:44:18 -0500

    create gitignore and delete student project files

--- Final/Drapings/footer.php
+++
@@ -1,8 +0,0 @@
-    </div>
-    <div id="pushdown"></div>
-    </div>
-    <div id="footer">
-	    <a href="http://goo.gl/zFVMZ">Cat Pictures</a> - <a href="#">Contact Me</a> - <a href="#">Terms of Use</a>
-	</div>	
-  </body>
-</html>
